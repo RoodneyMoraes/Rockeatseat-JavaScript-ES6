eb2259906e2251d8f4c4b13ac16316cef94f864a
Roodney Moraes <roodneysouza@gmail.com>
2020-02-26 22:11:49 -0300

    Estou fazendo o segundo modulo que é sobre o webpack

---
+++ 02-Webpack-Server/Aula01-configurando-webpack/funcoes.js
@@ -0,0 +1,4 @@
+// USANDO O EXPORT
+export function soma(a, b) {
+    return a + b;
+}
---
+++ 02-Webpack-Server/Aula01-configurando-webpack/main.js
@@ -0,0 +1,5 @@
+// USANDO O IMPORT
+import { soma } from "./funcoes";
+
+// EXIBINDO DADOS DO IMPORT
+console.log(soma(55, 7)); 
